1474d8ebdb07a36af59e24d94e03c575d23557b9
nagisheshero11 <hemanthsai964@gmail.com>
2026-02-25 10:57:22 +0530

    Added the moderator Logic and other verification operations

--- Contracts/src/IdentityRegistry.sol
+++ Contracts/src/IdentityRegistry.sol
@@ -1,26 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 contract IdentityRegistry {
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
 
-    address public admin;
-
-    mapping(address => bool) public isVerified;
-
-    constructor() {
-        admin = msg.sender;
+    struct User {
+        bool isVerified;
+        uint256 totalVolume;
+        uint256 riskScore;
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Not authorized");
-        _;
+    mapping(address => User) public users;
+
+    address public governmentSigner;
+
+    constructor(address _governmentSigner) {
+        governmentSigner = _governmentSigner;
     }
 
-    function verifyUser(address user) external onlyAdmin {
-        isVerified[user] = true;
+    function register(bytes memory signature) external {
+        require(!users[msg.sender].isVerified, "Already verified");
+
+        bytes32 messageHash = keccak256(abi.encodePacked(msg.sender));
+        bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
+
+        address recoveredSigner = ethSignedMessageHash.recover(signature);
+
+        require(
+            recoveredSigner == governmentSigner,
+            "Invalid government proof"
+        );
+
+        users[msg.sender].isVerified = true;
     }
 
-    function revokeUser(address user) external onlyAdmin {
-        isVerified[user] = false;
+    function isUserVerified(address user) external view returns (bool) {
+        return users[user].isVerified;
+    }
+
+    function updateVolume(address user, uint256 amount) external {
+        users[user].totalVolume += amount;
     }
 }
--- Contracts/src/PlatformToken.sol
+++ Contracts/src/PlatformToken.sol
@@ -3,36 +3,56 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IdentityRegistry.sol";
+import "./TransactionController.sol";
 
 contract PlatformToken is ERC20 {
 
     IdentityRegistry public identityRegistry;
+    TransactionController public transactionController;
 
     uint256 public transactionThreshold;
 
+    modifier onlyTransactionController() {
+        require(msg.sender == address(transactionController), "Not controller");
+        _;
+    }
+
     constructor(
         address _identityRegistry,
+        address _transactionController,
         uint256 _threshold
     ) ERC20("Hybrid Bank Token", "HBT") {
         identityRegistry = IdentityRegistry(_identityRegistry);
+        transactionController = TransactionController(_transactionController);
         transactionThreshold = _threshold;
 
         _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 
-    function _update(
+    function transfer(address to, uint256 amount) public override returns (bool) {
+
+        require(identityRegistry.isUserVerified(msg.sender), "User not verified");
+
+        if (amount >= transactionThreshold) {
+            transactionController.createPendingTransaction(
+                msg.sender,
+                to,
+                amount
+            );
+            return true;
+        }
+
+        identityRegistry.updateVolume(msg.sender, amount);
+
+        return super.transfer(to, amount);
+    }
+
+    function executeTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override {
+    ) external onlyTransactionController {
 
-        if (from != address(0) && amount >= transactionThreshold) {
-            require(
-                identityRegistry.isVerified(from),
-                "Sender not verified for high transaction"
-            );
-        }
-
-        super._update(from, to, amount);
+        _transfer(from, to, amount);
     }
 }
